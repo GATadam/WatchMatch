b3137ebe8058d9a5d60c90f48269e52a9f09501e
GATadam <102860473+GATadam@users.noreply.github.com>
2025-08-18 14:44:33 +0200

    a ciklusban töröltem, javítva

--- database_build/upload_movie_jsons_to_ftp.py
+++ database_build/upload_movie_jsons_to_ftp.py
@@ -5,20 +5,20 @@ from dotenv import load_dotenv
 
 
 def main(FTP_HOST, FTP_USER, FTP_PASS, movies_dir):
+    ftp = ftplib.FTP(FTP_HOST)
+    ftp.login(FTP_USER, FTP_PASS)
+    ftp.cwd("www/watchmatch_jsons/movies")
+    for file in ftp.nlst():
+        name = os.path.basename(file)
+        if name not in ('.', '..') and not file.endswith('/'):
+            try:
+                ftp.delete(file)
+            except ftplib.error_perm as e:
+                print(f"Could not delete {file}: {e}")
     for json_file in glob.glob(f"{movies_dir}/*.json"):
         with open(json_file, "rb") as f:
-            ftp = ftplib.FTP(FTP_HOST)
-            ftp.login(FTP_USER, FTP_PASS)
-            ftp.cwd("www/watchmatch_jsons/movies")
-            for file in ftp.nlst():
-                name = os.path.basename(file)
-                if name not in ('.', '..') and not file.endswith('/'):
-                    try:
-                        ftp.delete(file)
-                    except ftplib.error_perm as e:
-                        print(f"Could not delete {file}: {e}")
             ftp.storbinary(f"STOR {os.path.basename(json_file)}", f)
-            ftp.quit()
+    ftp.quit()
 
 
 if os.path.exists(".env"):
