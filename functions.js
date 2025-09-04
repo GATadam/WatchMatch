b3daf705037444c4a80909f40b8be6929c818240
GATadam <102860473+GATadam@users.noreply.github.com>
2025-09-04 10:11:19 +0200

    egységes const és ' használat

--- functions.js
+++ functions.js
@@ -3,25 +3,25 @@ const tabRegister = document.getElementById('tab_register');
 const loginPanel = document.getElementById('login');
 const registerPanel = document.getElementById('register');
 
-let emailLogin = document.getElementById('email_login');
-let passwordLogin = document.getElementById('password_login');
-let passwordRegister = document.getElementById('password_register');
+const emailLogin = document.getElementById('email_login');
+const passwordLogin = document.getElementById('password_login');
+const passwordRegister = document.getElementById('password_register');
 
 passwordLogin.style.width = emailLogin.offsetWidth + "px";
 passwordRegister.style.width = emailLogin.offsetWidth + "px";
 
 // a grid miatt kell a méret beállítás, mert a képet is egy gridelemnek venné
 
-let eyeIcons = document.querySelectorAll(".eye-icon");
+const eyeIcons = document.querySelectorAll('.eye-icon');
 eyeIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
+    icon.addEventListener('click', () => {
         let passwordInput = icon.previousElementSibling;
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            icon.src = "https://www.kosmicdoom.com/watchmatch_media/hide_pw.svg";
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            icon.src = 'https://www.kosmicdoom.com/watchmatch_media/hide_pw.svg';
         } else {
-            passwordInput.type = "password";
-            icon.src = "https://www.kosmicdoom.com/watchmatch_media/show_pw.svg";
+            passwordInput.type = 'password';
+            icon.src = 'https://www.kosmicdoom.com/watchmatch_media/show_pw.svg';
         }
     });
 });
